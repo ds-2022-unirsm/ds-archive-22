605587c81acfc0237dc2791bd1f908a1b5d6c307
chrbff <79915170+chrbff@users.noreply.github.com>
2022-04-04 12:43:01 +0200

    aggiunto walker noise

--- chrbff/p5/walker_noise/sketch.js
+++ chrbff/p5/walker_noise/sketch.js
@@ -4,6 +4,8 @@ var d = 300;
 var quanti = 3;
 var c = [];
 var speedMax = 4;
+var xoff = 0;
+let incr = 0;
 
 function setup() {
   createCanvas((w = windowWidth), (h = windowHeight));
@@ -41,19 +43,29 @@ function Camminatore(_id) {
   this.move = function () {
     if (random(1) >= 0.5) this.dirX *= -1;
     if (random(1) >= 0.5) this.dirY *= -1;
-    this.x += this.speed * this.dirX;
-    this.y += this.speed * this.dirY;
+
+    this.x += random(-1, 1) * map(noise(xoff), 0, 1, 0, 10);
+    this.y += random(-1, 1) * map(noise(xoff), 0, 1, 0, 10);
   };
 
   // metodo display
   this.display = function () {
     let shade = map(this.speed, 1, speedMax, 0, 255);
-    stroke(shade);
+    stroke(
+      map(noise(incr), 0, 1, 0, 255),
+      map(noise(incr), 0, 1, 0, 255),
+      map(noise(incr), 0, 1, 0, 255)
+    );
+
+    incr += 0.01;
+
     if (this.id == quanti - 1) this.qualeAltro = 0;
     else this.qualeAltro = this.id + 1;
     line(this.x, this.y, c[this.qualeAltro].x, c[this.qualeAltro].y);
     noStroke();
+
     fill(shade);
-    ellipse(this.x, this.y, 50);
+
+    ellipse(this.x, this.y, 20);
   };
 }
