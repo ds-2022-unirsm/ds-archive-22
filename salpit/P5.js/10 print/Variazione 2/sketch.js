f25bfef27a03bd58dd13d00cb26dfdee12aa1e35
Salvo Pitingaro <86733544+salpit@users.noreply.github.com>
2022-06-28 01:36:07 +0200

    Add licenze

--- salpit/P5.js/10 print/Variazione 2/sketch.js
+++ salpit/P5.js/10 print/Variazione 2/sketch.js
@@ -1,3 +1,7 @@
+//2022 © Salvo Pitingaro, Daniele @Fupete and the course DS-2022 at DESIGN.unirsm
+//github.com/ds-2022-unirsm — github.com/fupete
+//Educational purposes, MIT License, 2022, San Marino
+
 let cerchi = [];
 let numeroCerchi = 3;
 
