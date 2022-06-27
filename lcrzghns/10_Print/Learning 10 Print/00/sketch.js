2e17e94ffef604e3fd138d37b8658e314883d4d2
lcrzghns <101177495+lcrzghns@users.noreply.github.com>
2022-06-27 15:45:53 +0200

    Update sketch.js

--- lcrzghns/10_Print/Learning 10 Print/00/sketch.js
+++ lcrzghns/10_Print/Learning 10 Print/00/sketch.js
@@ -1,3 +1,16 @@
+//   _                    _               
+//  | | ___ _ __ ______ _| |__  _ __  ___ 
+//  | |/ __| '__|_  / _` | '_ \| '_ \/ __|
+//  | | (__| |   / / (_| | | | | | | \__ \
+//  |_|\___|_|  /___\__, |_| |_|_| |_|___/
+//                  |___/    
+//
+// 10 Print Base by Lucrezia Ghinassi [basic]
+// 2022 © Lucrezia Ghinassi, Daniele @Fupete and the course DS-2022 at DESIGN.unirsm 
+// github.com/ds-2022-unirsm — github.com/fupete
+// Educational purposes, MIT License, 2022, San Marino
+// —
+
 let x=0;
 let y=0;
 let w=20;
