f89e548062ac4819654f52e2f64938d57ae53dad
Salvo Pitingaro <86733544+salpit@users.noreply.github.com>
2022-06-28 01:36:49 +0200

    Add licenze

--- salpit/P5.js/10 print/Variazione 3/sketch.js
+++ salpit/P5.js/10 print/Variazione 3/sketch.js
@@ -6,6 +6,9 @@
 //    \::/  /   /:/  /  \:\__\  \/__/  \:\__\  \/__/
 //     \/__/    \/__/    \/__/          \/__/
 //-------------------------------------------------------
+//2022 © Salvo Pitingaro, Daniele @Fupete and the course DS-2022 at DESIGN.unirsm
+//github.com/ds-2022-unirsm — github.com/fupete
+//Educational purposes, MIT License, 2022, San Marino
 
 let arr = [];
 
